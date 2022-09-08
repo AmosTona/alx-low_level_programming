09d70a6efccc5f3d84d341741fa3dd287ff3a0b9
AmosTona <cheruiyotamos277@gmail.com>
2022-09-08 10:36:32 -0700

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  *
  *  * main - Entry point
- 
+ * 
  *    * Return: Always 0 (Success)
  *
  */
@@ -14,5 +14,4 @@ int main(void)
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
-
 }
