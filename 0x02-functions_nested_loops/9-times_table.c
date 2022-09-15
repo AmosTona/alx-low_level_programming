8e1c928ef3ce0ef1b61521f56eca70b55d13360e
AmosTona <cheruiyotamos277@gmail.com>
2022-09-14 20:26:43 -0700

    hot

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,24 +20,24 @@ void times_table(void)
 			tens = product / 10;
 			ones = product % 10;
 
-			 if (column == 0)
-			 {
-				 _putchar('0');
-			 }
-			 else if (product < 10)
-			 {
-				  _putchar(',');
-				  _putchar(' ');
-				  _putchar(' ');
-				  _putchar(ones + '0');
-			 }
-			 else
-			 {
-				  _putchar(',');
-				  _putchar(' ');
-				  _putchar(tens + '0');
-				  _putchar(ones + '0');
-			 }
+			if (column == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
 		}
 		_putchar('\n');
 	}
