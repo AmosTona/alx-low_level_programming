cb4cf25dea626e1c7c55807c1acf92458e4a0c5a
AmosTona <cheruiyotamos277@gmail.com>
2022-09-15 19:19:56 -0700

    hot

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,13 @@
 /**
  * _isupper - checks for uppercase character
  * @c: character that is checked
- * Return: if uppercase returns 1 otherwise return 0
+ * Return: if uppercase return 1 otherwise return 0
  */
 int _isupper(int c)
 {
 	if ((c >= 'A') && (c <= 'Z'))
 	{
 		return (1);
-		{
-	{
+	}
 	return (0);
 }
